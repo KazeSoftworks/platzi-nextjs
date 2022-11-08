30f1da6c66b1029a354917de52c0ce133e84a343
Spell Kaze <spellkaze@live.com>
2022-11-07 21:03:21 -0500

    ✨ Path aliases

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from 'components/Layout/Layout'
+import Layout from '@components/Layout/Layout'
 import { AppProps } from 'next/app'
 
 export default function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
